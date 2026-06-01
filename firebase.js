fd9b50d963901f74211198ccc64f9633e33d36f2
silva1655-boop <silva1655@gmail.com>
2026-06-01 17:06:00 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGYsVTMOVfklVkZ1Kzpld7mEEIIoISAtw",
@@ -8,10 +7,8 @@ const firebaseConfig = {
   projectId: "man-navimag",
   storageBucket: "man-navimag.firebasestorage.app",
   messagingSenderId: "873554402392",
-  appId: "1:873554402392:web:586c5ce9c79f9dc44c7e2d",
-  measurementId: "G-PCSE0QLBFV"
+  appId: "1:873554402392:web:586c5ce9c79f9dc44c7e2d"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db   = getFirestore(app);
-export const auth = getAuth(app);
+export const db = getFirestore(app);
